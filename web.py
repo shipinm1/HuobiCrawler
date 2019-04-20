4c8192d871215b42f462f04faea6e3c9c0281d1c
shipinm1 <shipin1@ualberta.ca>
2019-04-20 12:39:19 +0800

    core function

--- web.py
+++ web.py
@@ -4,14 +4,14 @@ import re
 import time
 
 def main():
-	totalPageNumber = getTotalPageNumber()
+	totalPageNumber, soup = getTotalPageNumber()
+	getInfoFromSoup(soup)
 	for i in range (2,totalPageNumber):
-		time.sleep(2)
+		time.sleep(3)
 		print("preparing page: " + str(i))
 		soup = rerest(i)
 		getInfoFromSoup(soup)
 		print("========================page: " + str(i) + "==========================")
-
 
 # get data from specific page number
 # return soup object
@@ -23,21 +23,16 @@ def rerest(pageNum):
 # get total  page number for itteration
 def getTotalPageNumber():
 	soup = rerest(1)
-	getInfoFromSoup(soup)
 	p = soup.select('p')
-	#matchObj = re.search( r'totalPage\W\W(.*)\W\WcurrPage', str(p))
+	#matchObj = re.search( r'totalPage\W\W(.*)\W\WcurrPage', str(p)) --------also works
 	matchObj = re.search(r'totalPage":(.*),"currPage', str(p))
 	if matchObj:
-		return int(matchObj.group(1))
+		return int(matchObj.group(1)), soup
 
 	else:
 		print("Page number No Found!")
-	
-	#testobj = re.search(r'totalPage":(.*),"currPage', str(p))
-	#print (testobj.group(1)) 
-
-	return 1
-
+		return 1, soup
+# get information from soup from each page.(minTradeLimit, maxTradeLimit, Price, )
 def getInfoFromSoup(soup):
 	matchObj = re.findall(r'minTradeLimit":(.*?),"maxTradeLimit', str(soup.select('p')))
 	for item in matchObj:
