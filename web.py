5d2de532f9b36ccb4edb5d9aff68984ee6b41ade
shipinm1 <shipin1@ualberta.ca>
2019-05-04 01:48:43 -0600

    with 0.1sec delay taking about 20sec to complete 16 pages scan

--- web.py
+++ web.py
@@ -4,14 +4,16 @@ import re
 import time
 
 def main():
+	startTime = time.time()
 	totalPageNumber, soup = getTotalPageNumber()
-	getInfoFromSoup(soup)
+	print("This is first page: ", getInfoFromSoup(soup)) #printing out the first page seperatly
 	for i in range (2,totalPageNumber):
-		time.sleep(3)
+		time.sleep(0.1)
 		print("preparing page: " + str(i))
 		soup = rerest(i)
-		getInfoFromSoup(soup)
-		print("========================page: " + str(i) + "==========================")
+		print("This is " + str(i) + " page: ", getInfoFromSoup(soup))
+	endTime = time.time()
+	print("Total time consume: ", endTime - startTime)
 
 # get data from specific page number
 # return soup object
@@ -20,7 +22,7 @@ def rerest(pageNum):
 	soup = BeautifulSoup(requests.get(start).content, features = "lxml")
 	return soup
 
-# get total  page number for itteration
+# get total  page number from first page for itteration
 def getTotalPageNumber():
 	soup = rerest(1)
 	p = soup.select('p')
@@ -32,9 +34,19 @@ def getTotalPageNumber():
 	else:
 		print("Page number No Found!")
 		return 1, soup
-# get information from soup from each page.(minTradeLimit, maxTradeLimit, Price, )
+
+# Get information from soup from each page.
+# Return a list of list which contain all the necessary numbers in the order of (UID, MIN, MAX, PRICE, AMOUNT)
 def getInfoFromSoup(soup):
-	matchObj = re.findall(r'minTradeLimit":(.*?),"maxTradeLimit', str(soup.select('p')))
+	matchObj = re.findall(r'"uid":(.*?),"userName"|minTradeLimit":(.*?),"isOnline"', str(soup.select('p')))
+	temp = 1
+	holder = []
 	for item in matchObj:
-		print(float(item))
+		if(temp == 1):
+			holder.append(list(item[0].split(' ')))
+			temp *= -1
+		else:
+			holder[-1].extend(re.findall(r'[-+]?\d*\.\d+|\d+', item[1]))
+			temp *= -1
+	return holder
 main()
